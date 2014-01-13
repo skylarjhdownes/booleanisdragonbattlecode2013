4bb68cfd1907de06a302a7b37e854733dda6b1a3
skylarjhdownes <skylarjhdownes@gmail.com>
2014-01-12 18:11:49 -0700

    Made a method to find the first empty square counterclockwise from the top, then used it to start spawning d00dz.

--- RobotPlayer.java
+++ RobotPlayer.java
@@ -1,23 +1,20 @@
 package potatos;
 
-import battlecode.common.Direction;
-import battlecode.common.GameConstants;
-import battlecode.common.RobotController;
-import battlecode.common.RobotType;
 import battlecode.common.*;
 import java.util.*;
 
-public class RobotPlayer{
-	
+public class RobotPlayer
+{
 	public static RobotController rc;
 	static Random randomThing = new Random();
 	
-	public static void run(RobotController rcin){
+	public static void run(RobotController rcin)
+	{
 		rc = rcin;
 		randomThing.setSeed(rc.getRobot().getID());
 		while(true){
 			try{
-				if(rc.getType()==RobotType.HQ){//if I'm a headquarters
+				if(rc.getType()==RobotType.HQ){
 					runHeadquarters();
 				}else if(rc.getType()==RobotType.SOLDIER){
 					runSoldier();
@@ -29,11 +26,39 @@ public class RobotPlayer{
 		}
 	}
 
-	private static void runSoldier() throws GameActionException {
+	private static void runSoldier() throws GameActionException 
+	{
 		
 	}
 	
-	private static void runHeadquarters() throws GameActionException {
+	private static void runHeadquarters() throws GameActionException 
+	{
+		Direction spawnDir = getFirstEmptySquareClockwiseFromTop();
+		System.out.println(spawnDir);
+		if(rc.isActive()&&rc.senseRobotCount()<GameConstants.MAX_ROBOTS&&spawnDir!=Direction.NONE){
+			rc.spawn(spawnDir);
+		}
 		
+	}
+	
+	private static Direction getFirstEmptySquareClockwiseFromTop() throws GameActionException 
+	{
+		Direction returnDir = Direction.NORTH_WEST;
+		int dirChangeCount = 0;
+		while (!rc.canMove(returnDir) && dirChangeCount < 8)
+		{
+			System.out.println(dirChangeCount);
+			returnDir = returnDir.rotateRight();
+			dirChangeCount++;
+		}
+		if(dirChangeCount>=8)
+		{
+			System.out.println("Potato");
+			return Direction.NONE;
+		}
+		else
+		{
+			return returnDir;
+		}
 	}
 }
